ba71febf96c5bc4d435054f5555e123a6a60a6a9
Eugene Alekseev <eugene.s.alekseev@gmail.com>
2017-12-17 17:00:02 +0300

    changed: Jinja static template to dynamic

--- static-routing/static-routing.py
+++ static-routing/static-routing.py
@@ -16,7 +16,12 @@ RSS_FEEDS = {
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed["entries"][0]
-    return flask.render_template("home.html")
+    return flask.render_template(
+            "home.html",
+            title=first_article.get("title"),
+            published=first_article.get("published"),
+            summary=first_article.get("summary")
+    )
 
 
 if __name__ == "__main__":
